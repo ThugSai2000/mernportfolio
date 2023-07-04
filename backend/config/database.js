bd2743a01429f01d621b61beb935832579be6d56
ThugSai2000 <125567260+ThugSai2000@users.noreply.github.com>
2023-07-04 11:09:20 +0530

    updated

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,6 +1,9 @@
 import mongoose from "mongoose";    
+import dotenv from "dotenv"
+
 
 export const connectDatabase = () => {
+    dotenv.config()
     mongoose
     .connect(process.env.MONGO_URI).then((c) =>{
         console.log(`Mongodb connect to: ${c.connection.host}`)

84c7a6c218087670d225f7cf0948c1567bd2de3d
ThugSai2000 <125567260+ThugSai2000@users.noreply.github.com>
2023-07-04 12:09:07 +0530

    update

--- backend/config/database.js
+++ backend/config/database.js
@@ -5,7 +5,7 @@ import mongoose from "mongoose";
 export const connectDatabase = () => {
    
     mongoose
-    .connect(`${process.env.MONGO_URI}`,{useNewUrlParser: true, useUnifiedTopology: true}).then((c) =>{
+    .connect(`${process.env.MONGO_URI}`).then((c) =>{
         console.log(`Mongodb connect to: ${c.connection.host}`)
     })
     .catch((e)=>{

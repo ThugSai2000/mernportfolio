25c6a33d6d54b0f143d4c6cd2c6a229a795821ca
ThugSai2000 <125567260+ThugSai2000@users.noreply.github.com>
2023-07-04 11:37:28 +0530

    updated

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,14 +1,23 @@
 import mongoose from "mongoose";    
-import dotenv from "dotenv"
 
 
-export const connectDatabase = () => {
-    dotenv.config()
-    mongoose
-    .connect(process.env.MONGO_URI).then((c) =>{
+
+export const connectDatabase = async () => {
+   try {
+   await mongoose
+    .connect(process.env.MONGO_URI, {
+        useUnifiedTopology:true,
+            useNewUrlParser: true,
+            useCreateIndex: true
+    }).then((c) =>{
         console.log(`Mongodb connect to: ${c.connection.host}`)
     })
-    .catch((e)=>{
+    
+   } catch (error) {
+              ((e)=>{
         console.log(e)
     })
+   }
+   
+    
 }
